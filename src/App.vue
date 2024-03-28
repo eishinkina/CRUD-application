1679ffa143a1d6a64f24305b3b76dddbb2118122
Ekaterina Ishinkina <esiforlearn@gmail.com>
2024-03-28 13:55:37 +0400

    Refactor App.vue to use PostForm and PostList components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-        v-model="title"
-        class="input"
-        type="text"
-        placeholder="Введите название поста"
-      />
-      <input
-        v-model="body"
-        class="input"
-        placeholder="Введите описание поста"
-      />
-      <button class="btn" @click="createPost">Добавить пост</button>
-    </form>
-    <div class="post" v-for="post in posts" :key="post.id">
-      <div><strong>Название: </strong>{{ post.title }}</div>
-      <div><strong>Описание: </strong>{{ post.body }}</div>
-    </div>
+    <PostForm @createPost="createPost" />
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
 export default {
+  components: {
+    PostForm,
+    PostList,
+  },
   data() {
     return {
       posts: [
@@ -48,15 +37,19 @@ export default {
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      this.title = "";
-      this.body = "";
+    // createPost() {
+    //   const newPost = {
+    //     id: Date.now(),
+    //     title: this.title,
+    //     body: this.body,
+    //   };
+    //   this.posts.push(newPost);
+    //   this.title = "";
+    //   this.body = "";
+    // },
+    createPost(post) {
+      post.id = Date.now();
+      this.posts.push(post);
     },
   },
 };
@@ -70,28 +63,5 @@ export default {
 }
 .app {
   padding: 20px;
-}
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <form @submit.prevent="emitCreatePost">
+      <h4>Создание поста</h4>
+      <input
+        v-model="title"
+        class="input"
+        type="text"
+        placeholder="Введите название поста"
+      />
+      <input
+        v-model="body"
+        class="input"
+        type="text"
+        placeholder="Введите описание поста"
+      />
+
+      <button class="btn">Добавить пост</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      body: "",
+    };
+  },
+  methods: {
+    emitCreatePost() {
+      if (this.title && this.body) {
+        // Добавлена простая проверка на пустые поля
+        this.$emit("createPost", { title: this.title, body: this.body });
+        this.title = "";
+        this.body = "";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
